504f9be1f8d93af70f388eee8a80829f07609ad3
Smushed <smushedcode@gmail.com>
2021-06-17 17:18:48 -0500

    Discord bot now supporting links

--- discord/dMessages.js
+++ discord/dMessages.js
@@ -4,26 +4,31 @@ const pingHandler = require('./handlers/pingHandler');
 
 module.exports = function (client) {
     client.on('message', message => {
-        if (message.content === '.help') {
-            pingHandler.help(message);
-            return;
-        };
-
-        if (message.channel.type !== 'dm') {
-            return;
-        };
-
-        if (message.content === '.id') {
-            pingHandler.replyId(message);
-
-        } else if (message.content.startsWith('a_cmd')) {
+        if (message.content.startsWith('a_cmd')) {
             const [x, cmdName, arg1, arg2] = message.content.split('-');
             if (cmdName === 'c_npc') {
                 npcHandler.createNPC(arg1); //arg1 should be name of NPC
             }
             // pingHandler.pingRyan(client);
             // guildHandler.saveUsers()
-        } else {
         };
+
+        if (!message.content.startsWith('.')) { return }
+
+        switch (message.content) {
+            case '.help': {
+                pingHandler.help(message);
+                break;
+            } case '.id': {
+                pingHandler.replyId(message);
+                break;
+            } case '.link': {
+                pingHandler.linkWebsite(message);
+                break;
+            } case '.messages': {
+                pingHandler.messagePage(message);
+                break;
+            }
+        }
     });
 }
--- discord/handlers/pingHandler.js
+++ discord/handlers/pingHandler.js
@@ -1,15 +1,33 @@
-const helpMessage = 'People who need help shouldn\'t be on the net \n .id - get ID for Kairos \n'
+const helpMessage = 'People who need help shouldn\'t be on the net \n.link - Link to Kairos\n.id - get ID for Kairos \n.messages - Your Messages'
+const Discord = require('discord.js');
+const db = require('../../models');
+
+const createEmbed = (title, url, description = '') => {
+    return new Discord.MessageEmbed()
+        .setTitle(title)
+        .setURL(url)
+        .setDescription(description);
+};
 
 module.exports = {
     pingRyan: async (client) => {
-        const ryan = await client.users.fetch(`106626827644059648`);
+        const ryan = await client.users.fetch('106626827644059648');
         const DMawait = await ryan.send('Cyberpunk is coming to life');
         console.log(DMawait)
     },
     help: (message) => {
-        message.author.send(`${helpMessage} \n ID: ${message.author.id}`);
+        message.author.send(`${helpMessage}`);
     },
     replyId: (message) => {
-        message.author.send(message.author.id)
+        message.author.send(message.author.id);
+    },
+    messagePage: async (message) => {
+        const user = await db.User.findOne({ DID: message.author.id });
+        const embedLink = createEmbed(`${user.N} Message Hub`, `http://neochicago.network/er/${message.author.id}`);
+        message.author.send('Here you are, Edgerunner', embedLink)
+    },
+    linkWebsite: (message) => {
+        const embedLink = createEmbed('Kairos Network', 'http://neochicago.network', 'NeoChicago\'s Edgerunner Hub');
+        message.author.send('', embedLink)
     }
 }
